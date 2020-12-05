f19f4ca0c599d75cc88f20f42fc9142e6d249e62
adventofcode17 <34220573+adventofcode17@users.noreply.github.com>
2020-12-05 06:47:44 -0800

    Day 5 - solution

---
+++ day_5/solution.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const fs = require("fs");
+
+// Sorted list of seat numbers
+const seats = fs.readFileSync("input.txt")
+    .toString()
+    .split("\n")
+    .map(getSeat)
+    .sort();
+
+// console.log(seats);
+
+function getIndex(s, topLetter) {
+    // Return the string's position e.g. getPosition("RRL", "R") returns 6 
+    return s.split("").reduce((acc, letter, i) => {
+        return acc + (letter === topLetter ? Math.pow(2, s.length - 1 - i) : 0);
+    }, 0);
+}
+
+function getSeat(pass) {
+    return (getIndex(pass.slice(0, 7), "B") * 8) + getIndex(pass.slice(7), "R");
+}
+
+function getEmptySeat() {
+    return seats.find((seat, i) => seat === seats[i + 1] - 2) + 1;
+}
+
+// 855, 552
+console.log(Math.max(...seats), getEmptySeat());
