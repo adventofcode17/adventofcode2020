2483aa9ebadf54d332df6c904959bb6937ff7d87
adventofcode17 <34220573+adventofcode17@users.noreply.github.com>
2020-12-07 14:53:24 -0800

    Day 7 - solution

---
+++ day_7/solution.js
@@ -0,0 +1,66 @@
+"use strict";
+
+const fs = require("fs");
+
+// {"blue": [[1, "green"], [2, "yellow"], ...], [...], ...}
+let rules = new Map(fs.readFileSync("input.txt")
+    .toString()
+    .split("\n")
+    .map(rule => rule.split(" bags contain "))
+    .map(([bag, innerBags]) => [bag, parse(innerBags)])
+);
+
+function parse(innerBags) {
+    if (innerBags === "no other bags.") {
+        return [];
+    }
+
+    const re = /^(?<count>[0-9]+) (?<colour>.+) bags?\.?$/;
+
+    return innerBags.split(", ").map(item => {
+        const parsedItem = item.match(re).groups;
+        return [parseInt(parsedItem.count), parsedItem.colour];
+    })
+}
+
+//console.log(rules);
+
+const goldBags = new Set();
+let size = -1;
+
+// Iteratively add colours to the list of bags that can contain gold
+// until we can add no more.
+while (goldBags.size !== size) {
+    size = goldBags.size;
+
+    rules.forEach((innerBags, colour) => {
+        if (goldBags.has(colour)) {
+            // Already confirmed this bag can contain gold
+            return
+        }
+
+        const goldInnerBags = innerBags.some(([_, innerColour]) => {
+            return innerColour === "shiny gold" || goldBags.has(innerColour)
+        })
+
+        if (goldInnerBags) {
+            goldBags.add(colour);
+        }
+    });
+}
+
+function countBags(colour) {
+    const childBags = rules.get(colour);
+
+    // Base case
+    if (childBags.length == 0) {
+        return 1;
+    }
+
+    // Recursive case
+    const totalChildBags = childBags.reduce((acc, [count, colour]) => acc + (count * countBags(colour)), 0)
+    return totalChildBags + 1;
+}
+
+// 139, 58175
+console.log(size, countBags("shiny gold") - 1);
