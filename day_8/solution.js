9b3b2908f75936ccaa3e6b8e745f60d757f96e29
adventofcode17 <34220573+adventofcode17@users.noreply.github.com>
2020-12-08 14:22:29 -0800

    Day 8 - part 2 solution

--- day_8/solution.js
+++ day_8/solution.js
@@ -4,59 +4,81 @@ const fs = require("fs");
 
 const re = /^(?<operation>[a-z]+) (?<value>[+-][0-9]+)$/;
 
-// {"blue": [[1, "green"], [2, "yellow"], ...], "red": [...], ...}
 let instructions = fs.readFileSync("input.txt")
     .toString()
     .split("\n")
     .map(instruction => instruction.match(re).groups)
-    .map(i => ({"operation": i.operation, "value": parseInt(i.value)}));
+    .map(i => ({ "operation": i.operation, "value": parseInt(i.value) }));
 
-console.log(instructions)
-
+// console.log(instructions)
 
 function run(instructions) {
-    // Run the given instructions until a loop is reached or an out-of-bounds instruction
-    // is attempted to be run.
+    // Run the given instructions until the program terminates, returning the current value in the accumulator and whether it terminates successfully.
 
     let accumulator = 0;
     let programCounter = 0;
     const instructionsRun = new Set();
-    
+
     while (!instructionsRun.has(programCounter)) {
         instructionsRun.add(programCounter);
-    
+
+        if (programCounter == instructions.length) {
+            // The program is supposed to terminate by attempting to execute an instruction immediately after the last instruction in the file.
+            return [accumulator, true];
+        } else if (programCounter < 0 || programCounter > instructions.length) {
+            // Invalid instruction. Exiting.
+            return [null, false];
+        }
+
         const instruction = instructions[programCounter];
-    
+
         // console.log(programCounter, accumulator, instruction)
-    
-        switch(instruction.operation) {
+
+        switch (instruction.operation) {
             // acc increases or decreases a single global value called the accumulator by the value given in the argument. For example, acc +7 would increase the accumulator by 7. The accumulator starts at 0. After an acc instruction, the instruction immediately below it is executed next.
             case "acc":
                 accumulator += instruction.value;
                 programCounter++;
                 break;
-    
+
             // jmp jumps to a new instruction relative to itself. The next instruction to execute is found using the argument as an offset from the jmp instruction; for example, jmp +2 would skip the next instruction, jmp +1 would continue to the instruction immediately below it, and jmp -20 would cause the instruction 20 lines above to be executed next.
             case "jmp":
                 programCounter += instruction.value;
                 break;
-    
+
             // nop stands for No OPeration - it does nothing. The instruction immediately below it is executed next.
             case "nop":
                 programCounter++;
                 break;
-    
+
             default:
-                console.log("Invalid operation. Exiting.");
-                return;
+                throw new TypeError("Invalid operation: " + instruction.operation);
         }
     }
 
-    return accumulator;
+    return [accumulator, false];
 }
 
 
-// 1548
-console.log(run(instructions))
+// Part 1 - 1548
+console.log(run(instructions)[0])
 
-//console.log(part1, part2);
+// Part 2 - 1375
+// Loop through each instruction in turn, testing if the transformation yields a valid program that terminates
+for (let i = 0; i < instructions.length; i++) {
+    let instruction = instructions[i];
+
+    if (instruction.operation === "acc") {
+        continue;
+    }
+
+    instruction.operation = instruction.operation === "jmp" ? "nop" : "jmp";
+
+    let [accumulator, terminatedOK] = run(instructions);
+    if (terminatedOK) {
+        console.log(accumulator);
+        break;
+    }
+
+    instruction.operation = instruction.operation === "jmp" ? "nop" : "jmp";
+}
