97905cf8027dd6bc5c170eed13625f2b6e336d1f
adventofcode17 <34220573+adventofcode17@users.noreply.github.com>
2020-12-09 12:21:52 -0800

    Day 9 - solution

---
+++ day_9/solution.js
@@ -0,0 +1,57 @@
+"use strict";
+
+const fs = require("fs");
+
+const values = fs.readFileSync("input.txt")
+    .toString()
+    .split("\n")
+    .map(n => parseInt(n));
+
+// console.log(values);
+
+const windowSize = 25;
+
+function valid(values, i) {
+    // Return whether the value at position i is valid
+    for (let j = 1; j <= windowSize; j++) {
+        for (let k = 1; k <= windowSize; k++) {
+            if (j == k) {
+                continue;
+            }
+
+            if (values[i] === values[i - j] + values[i - k]) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+// Part 1 - 731031916
+let invalidValue = null;
+
+for (let i = windowSize; i < values.length; i++) {
+    if (!valid(values, i)) {
+        invalidValue = values[i];
+        break;
+    }
+}
+console.log(invalidValue);
+
+// Part 2 - 93396727
+for (let i = 0; i < values.length - 1; i++) {
+    let sum = values[i], min = sum, max = sum;
+
+    for (let j = i + 1; j < values.length; j++) {
+        max = Math.max(max, values[j]);
+        min = Math.min(min, values[j]);
+
+        sum += values[j];
+        if (sum > invalidValue) {
+            break;
+        } else if (sum === invalidValue) {
+            console.log(min + max);
+            return;
+        }
+    }
+}
