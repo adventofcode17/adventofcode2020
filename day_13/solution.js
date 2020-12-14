b9b8a501957e29a820af7490c4bcf6c57113b8ab
adventofcode17 <34220573+adventofcode17@users.noreply.github.com>
2020-12-13 17:18:54 -0800

    Day 13 - part 2 solution

--- day_13/solution.js
+++ day_13/solution.js
@@ -9,14 +9,16 @@ let [minDeparture, ids] = fs.readFileSync("input.txt")
 minDeparture = parseInt(minDeparture);
 
 ids = ids.split(",")
-    .filter(id => id !== "x")
-    .map(id => parseInt(id));
+    .map((id, index) => [parseInt(id), index])
+    .filter(([id, _]) => !isNaN(id));
+
+console.log(ids);
 
 // What is the ID of the earliest bus you can take to the airport multiplied by the number of minutes you'll need to wait for that bus?
 let minWait = null;
 let answer = null;
 
-ids.forEach(id => {
+ids.forEach(([id, _]) => {
     const wait = id - (minDeparture % id);
     if (!minWait || (wait < minWait)) {
         minWait = wait;
@@ -26,3 +28,36 @@ ids.forEach(id => {
 
 // Part 1 - 136
 console.log(answer);
+
+
+/* Use Chinese remainder theorem
+
+The ids in the given and example inputs are pairwise co-prime positive integers(!!)
+
+https://en.wikipedia.org/wiki/Diophantine_equation#Chinese_remainder_theorem
+https://en.wikipedia.org/wiki/Chinese_remainder_theorem
+Good walkthrough of caluclation: https://www.omnicalculator.com/math/chinese-remainder
+Supports enough inputs to calculate answer: https://www.dcode.fr/chinese-remainder
+
+After trial/error with the simplest example 17,x,13,19 -> 3417,
+work out the right boxes to put numbers into and a final subtraction at the end :)
+
+Result: x=739320315996301
+
+Inputs:
+x=[index]mod[id]
+x≡0mod13
+x≡7mod37
+x≡13mod401
+x≡27mod17
+x≡32mod19
+x≡36mod23
+x≡42mod29
+x≡44mod613
+x≡85mod41
+
+Answer:
+(13 *37 *401 *17 *19 *23 *29 * 613 * 41) - 739320315996301 = 305068317272992
+
+Part 2 - 305068317272992
+*/
