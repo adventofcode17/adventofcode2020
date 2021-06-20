629c2197e57e8143f98eab29143bddb9aef3646e
adventofcode17 <34220573+adventofcode17@users.noreply.github.com>
2021-06-20 15:15:33 -0700

    Day 16 - large refactoring

--- day_16/solution.js
+++ day_16/solution.js
@@ -8,7 +8,7 @@ let input = fs.readFileSync("input.txt")
 
 input = parse(input);
 
-console.log(input);
+// console.log(input);
 
 function parseRule(ruleText) {
     const rulesRegex = /^(?<field>[^:]+): (?<ranges>.*)$/;
@@ -32,17 +32,19 @@ function parse(input) {
     // {field: [range]}
     const rules = input[0]
         .split("\n")
-        .reduce(function(acc, ruleText) {
+        .reduce(function (acc, ruleText) {
             const [field, ranges] = parseRule(ruleText);
             acc[field] = ranges;
             return acc;
         }, {});
 
+    // [103, 19, 278, ...]
     const ticket = input[1]
         .split("\n")[1]
         .split(",")
         .map(n => parseInt(n));
 
+    // [ticket]
     const nearbyTickets = input[2]
         .split("\n")
         .slice(1)
@@ -58,10 +60,10 @@ function parse(input) {
 }
 
 function isValidValue(value, ranges) {
-    // Return whether a value is valid for the given rule
+    // Return whether a value is valid for at least one of the given rule ranges
     for (const [min, max] of ranges) {
         if ((value >= min) && (value <= max)) {
-            // Found a range within the rule that matched
+            // Range matches
             return true;
         }
     }
@@ -71,107 +73,92 @@ function isValidValue(value, ranges) {
 }
 
 function sumErrors(ticket, rules) {
-    // Sum the values of any ticket which has no valid values, otherwise return zero.
+    // Sum the values of the ticket if it has no valid values, otherwise return zero.
     return ticket
         .filter(value => Object.entries(rules).every(([_, ranges]) => !isValidValue(value, ranges)))
         .reduce((a, b) => a + b, 0);
 }
 
-function errorRate(tickets) {
-    return tickets.reduce((acc, ticket) => acc + sumErrors(ticket, input["rules"]), 0);
+function errorRate(tickets, rules) {
+    return tickets.reduce((acc, ticket) => acc + sumErrors(ticket, rules), 0);
 }
-
-// 21980
-console.log(errorRate(input["nearbyTickets"]));
-
-// Part 2
 
 function isValidTicket(ticket, rules) {
     return ticket.every(value => Object.entries(rules).some(([_, ranges]) => isValidValue(value, ranges)));
 }
 
-
-const validTickets = input["nearbyTickets"].filter(ticket => isValidTicket(ticket, input["rules"]))
-console.log(validTickets)
-console.log(input["nearbyTickets"].length, validTickets.length);
-
-// Set of all field names, {"fieldOne", "fieldTwo", ...}
-const fieldNames = new Set(Object.keys(input["rules"]));
-console.log(fieldNames);
-
-// Possible fields per ticket column, [{"fieldOne"}, {"fieldOne, fieldThree"}, ...]
-let possibleFields = [];
-for (let i = 0; i < input["ticket"].length; i++) {
-    possibleFields.push(new Set(fieldNames));
-}
-
-for (const ticket of validTickets) {
-    for (let i = 0; i < ticket.length; i++) {
-        for (const field of new Set(possibleFields[i])) {
-            // console.log(ticket, i, field, input["rules"][field]);
-
-            if (!isValidValue(ticket[i], input["rules"][field])) {
-                possibleFields[i].delete(field);
+function compare(possibleFields, validTickets, rules) {
+    // Compare and filter (in place) the set of possible fields per ticket column with the given ticket ranges
+    for (const ticket of validTickets) {
+        for (let i = 0; i < ticket.length; i++) {
+            for (const field of new Set(possibleFields[i])) {
+                if (!isValidValue(ticket[i], rules[field])) {
+                    possibleFields[i].delete(field);
+                }
             }
         }
     }
 }
 
-// Now we have a reduced set of possible fields. Assume a single answer exists (or we'd infinite loop)
-// e.g. [[1, 2, 3], [1, 2], [1]] has the solution [3, 2, 1]
+function getAnyElement(s) {
+    // Return a single value from set s
+    return s.values().next().value;
+}
 
-// console.log(possibleFields);
-
-// TODO: tidy
 function knownFields(possibleFields) {
     const knownFields = new Set();
     for (const fields of possibleFields) {
         if (fields.size === 1) {
-            knownFields.add(getAnyElement(fields))
+            knownFields.add(getAnyElement(fields));
         }
     }
     return knownFields;
 }
 
-let known = knownFields(possibleFields);
+function reduce(possibleFields) {
+    // Reduce (in place) the set of possible fields to a single solution. Assumes the simple case that a single answer exists, otherwise will infinite loop
+    // e.g. [[1, 2], [1, 2, 3], [1]] has the solution [2, 3, 1]
 
-function getAnyElement(s) {
-    return s.values().next().value;
-}
+    let known = knownFields(possibleFields);
 
-let n = 0;
-while (known.size != possibleFields.length) {
-    // console.log(known)
+    while (known.size != possibleFields.length) {
+        possibleFields
+            .filter(fields => fields.size > 1)
+            .forEach(fields => {
+                for (let field of known) {
+                    fields.delete(field);
+                }
+            });
 
-    possibleFields
-        .filter(fields => fields.size > 1)
-        .forEach(fields => {
-            for (let field of known) {
-                // console.log(fields, known, field);
-                fields.delete(field);
-                // console.log(fields);
-            }
-        });
-
-    // console.log(possibleFields);
-    known = knownFields(possibleFields);
-    
-    n++
-    if (n > 25) {
-        console.log(possibleFields);
-        console.log(n);
-        break;
+        known = knownFields(possibleFields);
     }
 }
 
-console.log(possibleFields);
+function getFieldMappings(rules, ticket, nearbyTickets) {
+    // Return the list of valid field mappings [fieldOne, fieldThree, ...] for the given input
+    const validTickets = nearbyTickets.filter(ticket => isValidTicket(ticket, rules));
 
-let answer = 1;
-for (let i = 0; i < possibleFields.length; i++) {
-    if (getAnyElement(possibleFields[i]).startsWith("departure ")) {
-        answer = answer * input["ticket"][i];
+    // Set of all field names, {"fieldOne", "fieldTwo", ...}
+    const fieldNames = new Set(Object.keys(rules));
+
+    // Possible fields per ticket column, [{fieldOne}, {fieldOne, fieldThree}, ...]
+    let possibleFields = [];
+    for (let i = 0; i < ticket.length; i++) {
+        possibleFields.push(new Set(fieldNames));
     }
+
+    compare(possibleFields, validTickets, rules);
+    reduce(possibleFields);
+    return possibleFields.map(getAnyElement);
 }
+
+const fieldMappings = getFieldMappings(input["rules"], input["ticket"], input["nearbyTickets"]);
+
+let answer = fieldMappings
+    .reduce((acc, field, i) => field.startsWith("departure ") ? acc * input["ticket"][i] : acc, 1);
+
+// 21980
+console.log(errorRate(input["nearbyTickets"], input["rules"]));
 
 // 1439429522627
 console.log(answer);
